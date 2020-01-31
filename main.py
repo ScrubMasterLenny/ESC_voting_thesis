6b0d5039da03c8ee363c81bee2cf37c00ee57c6c
ScrubMasterLenny <40162979+ScrubMasterLenny@users.noreply.github.com>
2020-01-31 13:59:20 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from data_extraction import *
-from contest import *
-
-rule_1975_2015 = Rule([12,10,8,7,6,5,4,3,2,1], 10)
-ESC_classified = dict_to_classes(import_scores(), rule_1975_2015)
-
-for ESC in ESC_classified:
-    print("### ESC " + ESC + " ###")
-    ESC_classified[ESC].print_result()
